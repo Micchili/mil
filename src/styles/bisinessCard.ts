aec6f15b0ccd58dcdee2b1160b1e6f09a930e8c8
Miccili <wot.dannbo@gmail.com>
2019-11-09 00:22:35 +0900

    LayoutsのCSS作製

---
+++ src/styles/bisinessCard.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Article = styled.article`
+  width: 90vw;
+  height: 90vh;
+  background-color: #AAAAAA;
+  border-radius: 20px;
+  opacity: 0.8;
+`
