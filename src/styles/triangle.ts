1f10eab3db40c25725f57f400baa1e8df178d421
Miccili <wot.dannbo@gmail.com>
2019-11-09 00:21:20 +0900

    ｃｓｓ修正

---
+++ src/styles/triangle.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const TriangelDiv = styled.div`
+  position: relative;
+  width: 0;
+  height: 0;
+  left: 60%;
+  transform: rotate(-45deg);
+  border-style: solid;
+  border-width: 0 180px 280px 180px;
+  border-color: transparent transparent #ff9197 transparent;
+  opacity: 0.7;
+`
