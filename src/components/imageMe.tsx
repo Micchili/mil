1443b05e20ff76f979300ff3b4db9377592b9580
Miccili <wot.dannbo@gmail.com>
2019-11-11 23:17:30 +0900

    reduxを追加(使っていない)

--- src/components/imageMe.tsx
+++ src/components/imageMe.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react'
-import mil from '../image/mil.jpg'
-import { Image } from '../styles/imageMe'
+import{ Image }from '../styles/imageMe'
+import image from '../image/me3.jpg'
+// import { useDispatch , useSelector } from 'react-redux'
+// import { ImageUrl } from '../store/getImage'
 
 export const ImageMe: React.FC = () => {
+  // const getSelecter = (state: ImageUrl) => state.imageUrl
+  // const get = useSelector(getSelecter)
+  // const dispatch = useDispatch()
+
   return (
-    <Image src={mil} alt='michishita' />
+    <Image src={image} />
   )
 }
