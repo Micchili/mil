6818f83d53c5b32577884af78d437ed95b88fabd
Miccili <wot.dannbo@gmail.com>
2019-11-13 18:03:11 +0900

    layoutsで使う汎用のテキストコンポーネントを作製

---
+++ src/components/cardText.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { H2 } from '../styles/profession'
+
+type IProps = {
+  text: string
+}
+
+export const CardText: React.FC<IProps> = (props) => {
+  return (
+    <H2>{props.text}</H2>
+  )
+}
