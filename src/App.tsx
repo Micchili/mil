6b3eae3192a793c6c21a3d775d88abcee569a799
Miccili <wot.dannbo@gmail.com>
2019-11-19 00:55:19 +0900

    プロパイダー追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react'
 import * as Styled from './styles/App'
 import { Mil } from './pages/mil'
+import { createStore } from 'redux'
+import Flip from './store/isFlip'
+import { Provider } from 'react-redux'
+
+const store = createStore(Flip)
 
 const App: React.FC = () => {
   return (
-    <>
+    <Provider store={store}>
       <Styled.Global />
       <Mil />
-    </>
+    </Provider>
   )
 }
 
