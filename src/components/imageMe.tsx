7d591b43146635164750f8a30a3bd92974ee4261
Miccili <wot.dannbo@gmail.com>
2019-11-19 00:55:40 +0900

    不必要な処理排除

--- src/components/imageMe.tsx
+++ src/components/imageMe.tsx
@@ -1,14 +1,8 @@
 import * as React from 'react'
 import{ Image }from '../styles/imageMe'
 import image from '../image/mil.jpg'
-// import { useDispatch , useSelector } from 'react-redux'
-// import { ImageUrl } from '../store/getImage'
 
 export const ImageMe: React.FC = () => {
-  // const getSelecter = (state: ImageUrl) => state.imageUrl
-  // const get = useSelector(getSelecter)
-  // const dispatch = useDispatch()
-
   return (
     <Image src={image} />
   )
