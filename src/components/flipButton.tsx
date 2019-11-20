962d97de86e59ec627432be0a01c7fd08aa21fdb
Miccili <wot.dannbo@gmail.com>
2019-11-20 11:57:29 +0900

    flipButton作製

--- src/components/flipButton.tsx
+++ src/components/flipButton.tsx
@@ -2,11 +2,18 @@ import * as React from 'react'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faSync } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useSelector, useDispatch } from 'react-redux'
+import { isFlip } from '../store/isFlip'
 
 export const FlipButton: React.FC = () => {
+  const openSelecter = (state: isFlip) => state.flipped
+  const flipped = useSelector(openSelecter)
+  const dispatch = useDispatch()
   library.add(faSync)
+  const flip = flipped ? 'OPEN' : 'CLOSE'
+
   return (
-    <button>
+    <button onClick={() => dispatch({type: flip})}>
       <FontAwesomeIcon icon={faSync}  />
     </button>
   )
