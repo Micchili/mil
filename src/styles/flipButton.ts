ae2463c1dd71c510ed7a3c84b9d91439068a0ddb
Miccili <wot.dannbo@gmail.com>
2019-11-27 17:56:23 +0900

    FlipButtonのStyle追加

--- src/styles/flipButton.ts
+++ src/styles/flipButton.ts
@@ -1,5 +1,11 @@
 import styled from 'styled-components'
 
-export const CircleDiv = styled.button`
+export const Button = styled.button`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  outline: none;
+  padding: 0;
+  appearance: none;
   font-size: 30px;
 `
