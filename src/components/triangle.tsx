a333df8232cc5f0ce609d0e5e576c72c5844d238
Miccili <wot.dannbo@gmail.com>
2019-11-28 13:04:29 +0900

    使っていないコンポーネントを削除

--- src/components/triangle.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-import { TriangelDiv } from '../styles/triangle'
-
-export const Triangle = () => {
-  return (
-    <TriangelDiv />
-  )
-}
--- src/styles/circle.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const CircleDiv = styled.div`
-  width: 200px;
-  height: 200px;
-  border-radius: 200px;
-  background-color: #F0F060;
-  opacity: 0.6;
-`
