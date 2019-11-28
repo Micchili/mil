ce8f3d4867e0b662ef22729617ffed9e34d6a220
Miccili <wot.dannbo@gmail.com>
2019-11-28 12:57:40 +0900

    SkillTextコンポーネントを追加

---
+++ src/components/skillText.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import { Section, H2, P } from '../styles/skillText'
+
+type IProps = {
+  title?: string
+  text: string
+}
+
+export const SkillText: React.FC<IProps> = (props) => {
+  return (
+    <Section>
+      <H2>{props.title}</H2>
+      <P>{props.text}</P>
+    </Section>
+  )
+}
---
+++ src/styles/skillText.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components'
+
+export const Section = styled.section`
+  height: 90%;
+  border-radius: 20px;
+  background-color: #FAFAFA;
+
+  @media screen and (min-width: 1024px) {
+    width: 500px;
+    line-height: 40px;
+  }
+  @media screen and (min-width: 768px) and (max-width: 1024px) {
+    width: 300px;
+    line-height: 30px;
+  }
+  @media screen and (max-width: 767px) {
+    width: 120px;
+    line-height: 12px;
+  }
+`
+
+export const H2 = styled.h2`
+  font-size: 50px;
+  padding-left: 10px;
+
+  @media screen and (min-width: 1024px) {
+    font-size: 50px;
+  }
+  @media screen and (min-width: 768px) and (max-width: 1024px) {
+    font-size: 30px;
+  }
+  @media screen and (max-width: 767px) {
+    font-size: 17px;
+  }
+`
+
+export const P = styled.p`
+  font-size: 20px;
+
+  @media screen and (min-width: 1024px) {
+    font-size: 20px;
+  }
+  @media screen and (min-width: 768px) and (max-width: 1024px) {
+    font-size: 19px;
+  }
+  @media screen and (max-width: 767px) {
+    font-size: 12px;
+  }
+`
