8a3393ff5fd3f93604fcdfdc00ddd80607112f54
Miccili <wot.dannbo@gmail.com>
2019-11-28 12:59:16 +0900

    BackBisinessCardの修正とStyleの追加

--- src/molecules/backBisinessCard.tsx
+++ src/molecules/backBisinessCard.tsx
@@ -1,14 +1,71 @@
-import * as React from 'react'
-import { Article } from '../styles/bisinessCard'
+import * as React from 'react';
+import skills from '../text/text.json';
+import {
+  DivRight,
+  BackCardWarp,
+  DivCenter,
+  DivLeft,
+  SkillTextWrap,
+} from '../styles/backBisinesCard';
+import {CardText} from '../components/cardText';
+import {SkillBar} from '../components/skillBar';
+import {FlipButton} from '../components/flipButton';
+import {CloseButton} from '../components/closeButton';
+import {SkillText} from '../components/skillText';
 
-type IProps = {
-  style?: {}
-}
-
-export const BackBisinessCard: React.FC<IProps> = () => {
+export const BackBisinessCard: React.FC = () => {
+  const [discription, setDiscription] = React.useState(0);
   return (
-    <Article>
-      作っています
-    </Article>
-  )
-}
+    <BackCardWarp>
+      <CloseButton />
+      <DivCenter>
+        <DivLeft>
+          <CardText text={'Skills'} />
+          <SkillBar
+            text={'HTML/CSS'}
+            skill={90}
+            onClick={() => setDiscription(0)}
+          />
+          <SkillBar
+            text={'JavaScript'}
+            skill={70}
+            onClick={() => setDiscription(1)}
+          />
+          <SkillBar
+            text={'React'}
+            skill={70}
+            onClick={() => setDiscription(2)}
+          />
+          <SkillBar
+            text={'Vue'}
+            skill={60}
+            onClick={() => setDiscription(3)}
+          />
+          <SkillBar
+            text={'php'}
+            skill={70}
+            onClick={() => setDiscription(4)}
+          />
+          <SkillBar
+            text={'Laravel'}
+            skill={70}
+            onClick={() => setDiscription(5)}
+          />
+          <SkillBar
+            text={'FireBase'}
+            skill={30}
+            onClick={() => setDiscription(6)}
+          />
+        </DivLeft>
+      </DivCenter>
+      <DivCenter>
+        <DivRight>
+          <SkillTextWrap>
+            <SkillText title={skills.text.title[discription]} text={skills.text.context[discription]} />
+          </SkillTextWrap>
+          <FlipButton />
+        </DivRight>
+      </DivCenter>
+    </BackCardWarp>
+  );
+};
---
+++ src/styles/backBisinesCard.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+export const BackCardWarp = styled.article`
+  width: 95vw;
+  height: 95vh;
+  border-radius: 20px;
+  background-color: #DDDDDD;
+  display: flex;
+  justify-content: space-around;
+`
+
+export const DivLeft = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+`
+
+export const DivCenter = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+export const DivRight = styled.div`
+display: flex;
+justify-content: space-evenly;
+`
+export const SkillTextWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
