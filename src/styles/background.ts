669637aa4afddf120b8a9f127f5de136727dd615
Miccili <wot.dannbo@gmail.com>
2019-11-28 12:59:31 +0900

    BackGroundの修正

--- src/styles/background.ts
+++ src/styles/background.ts
@@ -4,8 +4,7 @@ export const BackGround = styled.div`
   width: 100vw;
   height: 100vh;
   display: flex;
+  justify-content: space-evenly;
   flex-direction: column;
-  background-image:linear-gradient(-45deg, #f5eeed 25%, #D0F0F0 25%, #D0F0F0 50%, #f5eeed 50%, #f5eeed 75%, #D0F0F0 75%, #D0F0F0);
-  background-size: 70px 70px;
-  background-attachment: fixed;
+  background-color: #F1FAFF;
 `
