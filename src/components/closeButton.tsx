bcb825827fa1f7b74ebfb78a147a8d57fe76793b
Miccili <wot.dannbo@gmail.com>
2019-11-28 10:20:49 +0900

    CloseButtonの修正

--- src/components/closeButton.tsx
+++ src/components/closeButton.tsx
@@ -3,12 +3,9 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faTimesCircle} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Button } from '../styles/closeButton'
-import { useSelector, useDispatch } from 'react-redux'
-import { AllState } from '../store/storeState'
+import { useDispatch } from 'react-redux'
 
 export const CloseButton: React.FC = () => {
-  const close = useSelector((state: AllState) => state.isOpenFade.isFade);
-  console.log(close)
   const dispatch = useDispatch()
   library.add(faTimesCircle)
 
