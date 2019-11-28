3a5e5ecaa1123d6fb1d0cce69ee56e5e1dec61ae
Miccili <wot.dannbo@gmail.com>
2019-11-28 10:19:51 +0900

    cardのlayoutsのStyle追加

---
+++ src/styles/card.ts
@@ -0,0 +1,28 @@
+import transition from 'styled-transition-group'
+
+export const CardWarp = transition.div`
+  width: 90vw;
+  height: 90vh;
+  border-radius: 20px;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+
+  &:enter {
+    opacity: 0.01;
+  }
+  &:enter-active {
+    opacity: 1;
+    transition: opacity 1000ms ease-in;
+  }
+  &:exit {
+    opacity: 1;
+  }
+  &:exit-active {
+    opacity: 0.01;
+    transition: opacity 800ms ease-in;
+  }
+`
