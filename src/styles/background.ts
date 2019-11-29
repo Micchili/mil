2830fb49a020e16c59ddb306b8818d9788511d14
Miccili <wot.dannbo@gmail.com>
2019-11-29 18:23:46 +0900

    Fadeした際にBackGroundを暗くする処理をスイカ

--- src/styles/background.ts
+++ src/styles/background.ts
@@ -1,10 +1,15 @@
 import styled from 'styled-components'
 
+type IProps = {
+  state: boolean
+}
+
 export const BackGround = styled.div`
   width: 100vw;
   height: 100vh;
   display: flex;
   justify-content: space-evenly;
   flex-direction: column;
-  background-color: #F1FAFF;
+  background-color: ${(props: IProps) => props.state ? "#000000" : "#F1FAFF" };
+  opacity: ${(props: IProps) => props.state ? "0.6" : "1" };
 `
