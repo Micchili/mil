ec509dae6fd5aad9e92d72691300cdeb06c36bce
Miccili <wot.dannbo@gmail.com>
2019-11-29 18:22:41 +0900

    storeの値を取得する処理を追加

--- src/pages/mil.tsx
+++ src/pages/mil.tsx
@@ -4,10 +4,15 @@ import { Card } from '../layouts/card'
 import { EnterButton } from '../components/enterButton'
 import { TopTitle } from '../components/topTitle'
 import { Div } from '../styles/mil'
+import { AllState } from '../store/storeState'
+import { useSelector } from 'react-redux'
 
 export const Mil: React.FC = () => {
+  const openSelecter = (state: AllState) => state.isOpenFade.isFade
+  const isOpen = useSelector(openSelecter)
+
   return (
-    <BackGround>
+    <BackGround state={isOpen}>
       <TopTitle text={'Portfolio'} />
       <Div>
         <EnterButton />
