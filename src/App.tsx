66fc8295d24b4a8df560917f8e933ab7de29ae59
Miccili <wot.dannbo@gmail.com>
2020-02-03 17:26:16 +0900

    ページ遷移時にアニメーションするように変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
-import * as React from 'react'
-import * as Styled from './styles/App'
-import { Mil } from './pages/mil'
-import { Provider } from 'react-redux'
-import { store } from './store/storeState'
-
-console.log(store.getState());
+import * as React from 'react';
+import {Provider} from 'react-redux';
+import * as Styled from './styles/App';
+import {Switch, Route, useLocation} from 'react-router-dom';
+import {animated, useTransition} from 'react-spring';
+import {Mil} from './pages/mil';
+import About from './pages/about'
+import {store} from './store/storeState';
 
 const App: React.FC = () => {
+  const location = useLocation();
+  const transitions = useTransition(location, location => location.pathname, {
+    from: {opacity: 0, transform: 'translate3d(100%,0,0)'},
+    enter: {opacity: 1, transform: 'translate3d(0%,0,0)'},
+    leave: {opacity: 0, transform: 'translate3d(-50%,0,0)'},
+  });
+
   return (
     <Provider store={store}>
       <Styled.Global />
-      <Mil />
+      {transitions.map(({key, item , props}) => (
+        <animated.div key={key} style={props}>
+          <Switch location={item}>
+            <Route path="/" exact={true} component={Mil} />
+            <Route path="/about" exact={true} component={About} />
+          </Switch>
+        </animated.div>
+      ))}
     </Provider>
-  )
-}
+  );
+};
 
 export default App;
