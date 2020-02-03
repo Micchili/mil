1d4d9fe1c00330dd757d38dfa791d59ad194f971
Miccili <wot.dannbo@gmail.com>
2020-02-03 17:31:24 +0900

    TopTitleを大幅修正、文字に色を付け、切り出した

--- src/styles/topTitle.ts
+++ src/styles/topTitle.ts
@@ -1,13 +1,18 @@
 import styled from 'styled-components'
 
 export const H1 = styled.h1`
+  color: #ACB6E5;
+  background: linear-gradient(0deg, #ACB6E5, #86FDE8);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+
   @media screen and (min-width: 1024px) {
-    font-size: 100px;
+    font-size: 9rem;
   }
   @media screen and (min-width: 768px) and (max-width: 1024px) {
-    font-size: 70px;
+    font-size: 5rem;
   }
   @media screen and (max-width: 767px) {
-    font-size: 50px;
+    font-size: 3rem;
   }
 `
