8aef4d44c94145b8f1858dd944bdfc7cdc792a42
Micchili <wot.dannbo@gmail.com>
2020-03-16 06:25:34 +0900

    Heightの値を変更

--- src/styles/snsLink.ts
+++ src/styles/snsLink.ts
@@ -4,23 +4,23 @@ export const DivWraperSnsLink = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  border-left: solid 3px #ffffff;
+  border-left: solid 2px #ffffff;
 
   @media screen and (min-width: 1024px) {
-    height: 300px;
-    font-size: 45px;
+    height: 120px;
+    font-size: 18px;
     padding: 10px;
-    flex-direction: column;
-    justify-content: space-between;
   }
   @media screen and (min-width: 768px) and (max-width: 1024px) {
     height: 150px;
-    font-size: 40px;
-    flex-direction: column;
-    justify-content: space-between;
+    font-size: 37px;
   }
   @media screen and (max-width: 767px) {
     height: 80px;
-    font-size: 15px;
+    font-size: 14px;
   }
+`
+
+export const A = styled.a`
+  margin-left: 5px;
 `
