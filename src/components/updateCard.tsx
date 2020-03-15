eafe00c152bef90aa4babfde2fe24ba96fb1fd08
Micchili <wot.dannbo@gmail.com>
2020-03-16 06:26:02 +0900

    UpdateCardコンポーネントを追加

---
+++ src/components/updateCard.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import { Artcale, P } from '../styles/updateCard'
+
+export const UpdateCard: React.FC = () => {
+  return (
+    <Artcale>
+      <P>11/29 Portforio公開</P>
+      <P>11/30 細かい修正</P>
+    </Artcale>
+  )
+}
---
+++ src/styles/updateCard.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Artcale = styled.article`
+  width: 50%;
+  height: 50%;
+  background-color: #FAFAFA;
+`
+
+export const P = styled.p`
+  font-size: 20px;
+`
