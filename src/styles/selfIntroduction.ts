4817f60c18d9c953eb96ad467c4498477d5cff1c
Micchili <wot.dannbo@gmail.com>
2020-03-16 06:29:16 +0900

    SelfIntroductionのStyleを追加

---
+++ src/styles/selfIntroduction.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+
+export const SelfWrap = styled.div`
+  width: 100vw;
+  display: flex;
+  justify-content: space-evenly;
+`
+
+export const SelfImageLeft = styled.article`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+export const SelfTextRight = styled.article`
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+`
+
+export const Paragraph = styled.article`
+  width: 100%;
+  height: 100%;
+`
+
+export const H1 = styled.h1`
+  font-size: 3rem;
+  font-weight: bold;
+`
+
