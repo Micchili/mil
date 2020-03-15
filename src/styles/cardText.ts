c1e63abb31f5d0d7fd2756d3102fe91ce0323e59
Micchili <wot.dannbo@gmail.com>
2020-03-16 06:30:16 +0900

    Font-Sizeを変更

--- src/styles/cardText.ts
+++ src/styles/cardText.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const H2 = styled.h2`
   @media screen and (min-width: 1024px) {
-    font-size: 50px;
+    font-size: 24px;
   }
   @media screen and (min-width: 768px) and (max-width: 1024px) {
     font-size: 30px;
