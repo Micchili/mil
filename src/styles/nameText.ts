6292a3f4422f22c7b6d0aef5ccf6965fd20ad6e9
Micchili <wot.dannbo@gmail.com>
2020-03-16 06:30:47 +0900

    各ブレークポイントにFont-Sizeを追加

--- src/styles/nameText.ts
+++ src/styles/nameText.ts
@@ -1,16 +1,15 @@
 import styled from 'styled-components'
 
 export const Name = styled.h2`
-  font-size: 100px;
   padding-bottom: 5px;
 
   @media screen and (min-width: 1024px) {
-    font-size: 100px;
+    font-size: 56px;
   }
   @media screen and (min-width: 768px) and (max-width: 1024px) {
-    font-size: 50px;
+    font-size: 40px;
   }
   @media screen and (max-width: 767px) {
-    font-size: 50px;
+    font-size: 40px;
   }
 `
