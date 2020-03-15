727d7507cded85e4d85b3874d618167296b02649
Micchili <wot.dannbo@gmail.com>
2020-03-16 06:28:15 +0900

    自己紹介ページのStyleを追加

---
+++ src/styles/about.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const AboutWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: #ffe6e9;
+`
