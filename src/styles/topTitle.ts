a975d3dbb92afe361d6ca1408ed5782c13268a57
Micchili <wot.dannbo@gmail.com>
2020-04-23 07:51:59 +0900

    Headerのタイトルを大きくするように変更

--- src/styles/topTitle.ts
+++ src/styles/topTitle.ts
@@ -1,16 +1,27 @@
 import styled from 'styled-components'
 
 export const H1 = styled.h1`
-  color: #ACB6E5;
-  background: linear-gradient(0deg, #ACB6E5, #86FDE8);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
+  position: relative;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  font-family: -apple-system, BlinkMacSystemFont, avenir next, avenir, helvetica neue, helvetica, ubuntu, roboto, noto,
+    segoe ui, arial, sans-serif;
+  /* color: #ACB6E5; */
+  color: #0068B7;
+  background: transparent;
+  font-weight: 700;
+  will-change: transform, opacity;
+  text-shadow: 0px 2px 40px #00000020, 0px 2px 5px #00000030;
+
 
   @media screen and (min-width: 1024px) {
-    font-size: 9rem;
+    font-size: 13rem;
   }
   @media screen and (min-width: 768px) and (max-width: 1024px) {
-    font-size: 5rem;
+    font-size: 10rem;
   }
   @media screen and (max-width: 767px) {
     font-size: 3rem;
