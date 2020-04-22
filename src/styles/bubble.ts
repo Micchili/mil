89a4b0587ec405f9c6bc5c940701875a58afb303
Micchili <wot.dannbo@gmail.com>
2020-04-23 07:50:03 +0900

    泡が出てくる際のAnimation挙動を変更

--- src/styles/bubble.ts
+++ src/styles/bubble.ts
@@ -3,7 +3,10 @@ import styled, {keyframes} from 'styled-components';
 const BubbleAnimation = keyframes`
   0% {
     bottom: 0;
-    transform: translateX(0);
+    transform: scale(0,0) translate(0%, 0%);
+  }
+  5% {
+    transform: scale(1.0,1.0) translateX(0);
   }
   50% {
     transform: translateX(100px);
@@ -14,17 +17,19 @@ const BubbleAnimation = keyframes`
 `;
 
 export const Bubble = styled.div`
-  position: absolute;
+  position: relative;
   bottom: 0;
   width: 40px;
   height: 40px;
-  background: linear-gradient(#05FBFF, #1E00FF);
+  border-style: solid;
+  border-width: 3px;
+  border-color: #ACB6E5;
   border-radius: 50%;
   animation: ${BubbleAnimation} 10s ease-in infinite;
 
   &:nth-child(1) {
-    width: 20px;
-    height: 20px;
+    width: 40px;
+    height: 40px;
     left: 30%;
     animation-duration: 6s;
   }
@@ -42,16 +47,23 @@ export const Bubble = styled.div`
   }
 
   &:nth-child(4) {
-    width: 50px;
-    height: 50px;
+    width: 100px;
+    height: 100px;
     left: 67%;
     animation-duration: 6s;
   }
 
   &:nth-child(5) {
-    width: 20px;
-    height: 20px;
-    left: 70%;
+    width: 30px;
+    height: 30px;
+    left: 83%;
     animation-duration: 4.5s;
+  }
+
+  &:nth-child(6) {
+    width: 32px;
+    height: 32px;
+    left: 56%;
+    animation-duration: 10s;
   }
 `;
