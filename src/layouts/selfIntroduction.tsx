8f2a0988cfdfa7d1b9b701bd3f85b316e0b5aa6e
Micchili <wot.dannbo@gmail.com>
2020-04-23 07:49:07 +0900

    文字を大きくするように変更

--- src/layouts/selfIntroduction.tsx
+++ src/layouts/selfIntroduction.tsx
@@ -5,6 +5,7 @@ import {
   SelfTextRight,
   H1,
   Paragraph,
+  P,
 } from '../styles/selfIntroduction';
 import {ImageMe} from '../components/imageMe';
 
@@ -17,26 +18,18 @@ const SelfIntroduction: React.FC = () => {
       <SelfTextRight>
         <Paragraph>
           <H1>プロフィール</H1>
-          <p>道下 潤</p>
-          <p>HAL東京 Web開発学科 ３年</p>
+          <P>道下 潤</P>
+          <P>HAL東京 Web開発学科 4年</P>
         </Paragraph>
         <Paragraph>
-          <p>
+          <P>
             平成10年10月4日、大都市横浜のとある病院で爆誕。
             中学で進路を決める際に「お米を作りたい」という思いから農業高校へ。
-            授業を受けているうちに農家の現状を知り絶望、PCを普段からいじっていたため、プログラマーを目指す事を決意。
-            高校卒業後、専門学校HAL東京へ入学、高度情報処理学科へ。
+            授業を受けているうちに農家の現状を知り絶望、プログラマーを目指す事を決意。
+            高校卒業後に専門学校HAL東京へ入学、高度情報処理学科へ。
             2年時に触れたPHPでのWeb開発が最高に楽しく、Webへの道を進み始める。
-            3年時、Web開発学科へと進学し、今に至る。
-          </p>
-        </Paragraph>
-        <Paragraph>
-          <p>
-            フロントエンドエンジニアを目指しています。
-            今まで経験してきた言語はHTML5/CSS3,PHP,JavaScript,Cです。
-            普段書いている言語はReactとTypeScriptです。
-            このサイトはFireBaseのFireStoreとHostingで構築しています。
-          </p>
+            3年時にWeb開発学科へと進学し、今に至る。
+          </P>
         </Paragraph>
       </SelfTextRight>
     </SelfWrap>
--- src/styles/selfIntroduction.ts
+++ src/styles/selfIntroduction.ts
@@ -1,9 +1,12 @@
 import styled from 'styled-components'
 
 export const SelfWrap = styled.div`
-  width: 100vw;
+  width: 99vw;
+  height: 800px;
   display: flex;
+  align-items: center;
   justify-content: space-evenly;
+  background-color: #F5F5DC;
 `
 
 export const SelfImageLeft = styled.article`
@@ -15,10 +18,11 @@ export const SelfImageLeft = styled.article`
 
 export const SelfTextRight = styled.article`
   width: 300px;
+  height: 350px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: flex-start;
+  justify-content: space-evenly;
 `
 
 export const Paragraph = styled.article`
@@ -27,7 +31,12 @@ export const Paragraph = styled.article`
 `
 
 export const H1 = styled.h1`
-  font-size: 3rem;
-  font-weight: bold;
+  font-size: 2.8rem;
+  font-weight: 800;
+`
+
+export const P = styled.p`
+  font-size: 1.2rem;
+  font-weight: 600;
 `
 
